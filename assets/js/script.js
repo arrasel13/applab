6dc4bb74038a7159ab2e5f6568c0c18526756023
AR Rasel <rasel403@gmail.com>
2022-07-01 11:28:29 +0600

    applab dev
    without responsiveness

---
+++ assets/js/script.js
@@ -0,0 +1,13 @@
+// Custom JS code here
+
+// Tabs JS
+$( function() {
+    $( "#tabs" ).tabs();
+} );
+
+// Accordion JS
+$( function() {
+    $( "#accordion" ).accordion({
+      collapsible: true
+    });
+} );
